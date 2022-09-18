5b83f583b2675afe990acc1a00fe1a8144553618
Bright <brightiniabasi@gmail.com>
2022-09-18 01:14:02 +0100

    Code review changes :ok_hand:

--- models/base_model.py
+++ models/base_model.py
@@ -17,14 +17,34 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != "__class__":
-                    setattr(self, key, value)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            if 'update_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                                        kwargs['updated_at'],
+                                        '%Y-%m-%dT%H:%M:%S.%f'
+                                        )
+            else:
+                kwargs['updated_at'] = datetime.now()
+
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(
+                                        kwargs['created_at'],
+                                        '%Y-%m-%dT%H:%M:%S.%f'
+                                        )
+            else:
+                kwargs['created_at'] = datetime.now()
+
+            if 'id' not in kwargs:
+                kwargs['id'] = str(uuid.uuid4())
+
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -50,7 +70,7 @@ class BaseModel:
         if dictionary.get('_sa_instance_state'):
             del dictionary['_sa_instance_state']
 
-        return 
+        return dictionary
 
     def delete(self):
         """Delete the current instance from storage."""
